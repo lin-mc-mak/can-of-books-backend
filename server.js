27c5ad7873ce6b0cd7ea15dedfa69417907ce6e6
Michael Maker <maker.mdm@gmail.com>
2022-03-29 17:01:01 -0500

    Add console.log

--- server.js
+++ server.js
@@ -15,16 +15,14 @@ const PORT = process.env.PORT || 3002;
 mongoose.connect(process.env.DB_URL);
 
 app.get('/books', async (request, response) => {
-
   const filterQuery = {};
   if(request.query.title){
     filterQuery.title = request.query.title;
   }
-
   const books = await Book.find(filterQuery);
+  // console.log(books);
   response.send(books);
 });
-
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
