0baff933c42eaa5fc91c57799f5d21b85d29dbfa
Michael Maker <maker.mdm@gmail.com>
2022-04-02 14:08:39 -0500

    Update status data type in seed data

--- seed.js
+++ seed.js
@@ -10,21 +10,21 @@ async function seed() {
   await Book.create({
     title: 'The Way of Kings',
     description: 'Stormlight Archive: First Book',
-    status: 'Published',
+    status: 'True',
     email: 'user1@yahoo.com',
   });
   console.log('first book added'); 
   await Book.create({
     title: 'Surely You\'re Joking, Mr. Feynman!',
     description: 'Richard Feynman (1918-1988), winner of the Nobel Prize in physics, thrived on outrageous adventures. Here he recounts in his inimitable voice his experience trading ideas on atomic physics with Einstein and Bohr and ideas on gambling with Nick the Greek; cracking the uncrackable safes guarding the most deeply held nuclear secrets; painting a naked female toreador—and much else of an eyebrow-raising nature.',
-    status: 'Published',
+    status: 'False',
     email: 'user2@yahoo.com',
   });
   console.log('second book added'); 
   await Book.create({
     title: 'Harry Potter and the Sorcerers Stone',
     description: 'You\'re A Wizard, Harry',
-    status: 'Published',
+    status: 'True',
     email: 'HPotter3@wizard.com',
   });
   console.log('all books added'); 
