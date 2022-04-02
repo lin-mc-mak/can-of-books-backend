243d973b4e0d2538232866aa74fad9d5e4f09d67
Michael Maker <maker.mdm@gmail.com>
2022-04-02 14:08:58 -0500

    Add route and function to DELETE books

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const Book = require("./models/book");
 const mongoose = require("mongoose");
-const { request } = require("express");
-const res = require("express/lib/response");
+// const res = require("express/lib/response");
 const app = express();
 app.use(cors());
 
@@ -16,34 +15,27 @@ const PORT = process.env.PORT || 3002;
 
 mongoose.connect(process.env.DB_URL);
 
-app.get("/books", async (request, response) => {
-  const filterQuery = {};
-  if (request.query.title) {
-    filterQuery.title = request.query.title;
-  }
-  if (request.query.email) {
-    filterQuery.email = request.query.email;
-  }
-  const books = await Book.find(filterQuery);
-  // console.log(books);
-  response.send(books);
-});
-
+app.get("/books", getBooks);
 app.post("/books", postBook);
 app.delete("/books/:id", deleteBook);
 
-//the event on the front end should take user input of email and put it in the req body
-async function deleteBook(request, response, next) {
-  let id = request.params.id;
+async function getBooks(request, response, next) {
   try {
-      console.log(`id to delete: `, id);
-     let deletedBook = await Book.findByIdAndDelete(id);
-      response.send(deletedBook);
+    const filterQuery = {};
+    if (request.query.title) {
+      filterQuery.title = request.query.title;
+    }
+    if (request.query.email) {
+      filterQuery.email = request.query.email;
+    }
+    const books = await Book.find(filterQuery);
+    response.send(books);
   } catch (error) {
     next(error);
   }
 }
 
+// FUNCTION TO POST A NEW BOOK
 async function postBook(request, response, next) {
   console.log(request.body);
   try {
@@ -54,17 +46,23 @@ async function postBook(request, response, next) {
   }
 }
 
+// FUNCTION TO DELETE A BOOK
+//the event on the front end should take user input of email and put it in the req body
+async function deleteBook(request, response, next) {
+  let id = request.params.id;
+  try {
+    console.log(`id to delete: `, id);
+    let deletedBook = await Book.findByIdAndDelete(id);
+    response.send(deletedBook);
+  } catch (error) {
+    next(error);
+  }
+}
+
 //for error handling:
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
+  next(error);
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
-
-  // Book.findOne({currentEmail}, (err, person) => {
-  //   if (err){
-  //     console.error(err); 
-  //     const newBookArr = person.
-  //   } 
-  // })
